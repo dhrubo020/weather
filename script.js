e313df53359ed4da10eb1ed18ac793031a868eb2
dhrubo020 <44555051+dhrubo020@users.noreply.github.com>
2020-08-10 02:19:42 +0600

    Update script.js

--- script.js
+++ script.js
@@ -33,7 +33,7 @@ function initMap() {
     var uluru = {lat: lat, lng: lon};
     // The map, centered at Uluru
     var map = new google.maps.Map(
-        document.getElementById('map'), {zoom: 4, center: uluru});
+        document.getElementById('map'), {zoom: 10, center: uluru});
     // The marker, positioned at Uluru
     var marker = new google.maps.Marker({position: uluru, map: map});
   }
